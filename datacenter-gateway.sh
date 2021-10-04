b71b7ffc61cca2b70553164216933a416ba1fd74
eRQee <rizky@prihanto.web.id>
2021-10-04 08:13:25 +0700

    update comment & explanation

--- datacenter-gateway.sh
+++ datacenter-gateway.sh
@@ -5,16 +5,19 @@
 # Required-Stop:     $remote_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: local network/firewall scripts
-# Description:       apply the init of local network/firewall scripts at boot time.
-# Author:            eRQee (q@serverq.org)
+# Short-Description: NAT Network & firewall scripts
+# Description:       apply the initialization of local NAT Network definition & firewall scripts at boot time.
+# Author:            eRQee (rizky@prihanto.web.id)
 ### END INIT INFO
 
 PATH=/usr/sbin:/sbin:/bin:/usr/bin
 
-# declaring interfaces
-outif="vmbr0" # the ethernet card connected to the internet
-lanif="vmbr1" # the one connected to the lan
+###
+### EDIT THIS SEGMENT with the OS identity of network interfaces, e.g. eth0, ens18, wlan0, wlp4s0, vmbr0, etc. (see ifconfig)
+###
+#
+outif="vmbr0" # the network card connected to the internet
+lanif="vmbr1" # the one that connected to the lan/vlan and/or need to be defined as the nodes behind NAT
 
 # delete all existing rules.
 iptables -F
@@ -31,7 +34,7 @@ iptables -A INPUT -m state --state NEW ! -i $outif -j ACCEPT
 iptables -A FORWARD -i $outif -o $lanif -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 # Drop all null packets
-# iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
 
 # Allow outgoing connections from the LAN side
 iptables -A FORWARD -i $lanif -o $outif -j ACCEPT
@@ -43,20 +46,19 @@ iptables -t nat -A POSTROUTING -o $outif -j MASQUERADE
 # Don't forward from the outside to the inside
 iptables -A FORWARD -i $outif -o $outif -j REJECT
 
-# allow the DHCP server
-iptables -A INPUT -s 172.57.58.0/24 -i $outif -p tcp --sport 68 --dport 67 -j ACCEPT
-iptables -A INPUT -s 175.57.58.0/24 -i $outif -p udp --sport 68 --dport 67 -j ACCEPT
+# allow the DHCP server, or disable this 2 line below if you rather not to implement this gateway as DHCP provider.
+#### pre-requisites: dnsmasq -- install with : apt install -y dnsmasq 
+#### please change the 192.168.0.0 with the subnet address that you use
+# 
+iptables -A INPUT -s 192.168.0.0/24 -i $outif -p tcp --sport 68 --dport 67 -j ACCEPT
+iptables -A INPUT -s 192.168.0.0/24 -i $outif -p udp --sport 68 --dport 67 -j ACCEPT
 
-######################################
-# Port Forwarding to local resources #
-######################################
+##########################################
+# DMZ/Port Forwarding to local resources #
+##########################################
 
-#iptables -t nat -A PREROUTING -p tcp --dport 22105 -j DNAT --to-destination 172.57.58.105:22            #SSH tunnel to VM105
-#iptables -t nat -A PREROUTING -p tcp --dport 22106 -j DNAT --to-destination 172.57.58.106:22            #SSH tunnel to VM106
-#iptables -t nat -A PREROUTING -p tcp --dport 22108 -j DNAT --to-destination 172.57.58.108:22            #SSH tunnel to VM108
-#iptables -t nat -A PREROUTING -p tcp --dport 22109 -j DNAT --to-destination 172.57.58.109:22            #SSH tunnel to VM109
-#iptables -t nat -A PREROUTING -p tcp --dport 22111 -j DNAT --to-destination 172.57.58.111:22            #SSH tunnel to VM111
-#iptables -t nat -A PREROUTING -p tcp --dport 25432 -j DNAT --to-destination 172.57.58.106:5432          #SSH Anggota.B.ID PostgreSQL Direct
+# example:
+#iptables -t nat -A PREROUTING -p tcp --dport 22100 -j DNAT --to-destination 192.168.0.100:22 #SSH tunnel example to VM100 via port 22100
 
 ######################################
 # Enable the Routes                  #
